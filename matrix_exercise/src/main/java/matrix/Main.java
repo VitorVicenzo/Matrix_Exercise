78d7afda88e9315f63f7378dc48a73cde34faa7e
VitorVicenzo <vitorvicenzo70@gmail.com>
2026-04-01 17:34:00 -0300

    Update Main.java

--- matrix_exercise/src/main/java/matrix/Main.java
+++ matrix_exercise/src/main/java/matrix/Main.java
@@ -5,5 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+
+        
+
+
+        sc.close();
     }
 }
