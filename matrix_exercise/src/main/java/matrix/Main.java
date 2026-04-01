d50decdc9e0942d0f66fde7edea99fc67a6b5dd5
VitorVicenzo <vitorvicenzo70@gmail.com>
2026-04-01 18:25:08 -0300

    Use Integer matrix and add neighbor checks
    Change matrix type from int[][] to Integer[][] and switch loop counters to Integer. Fix inner loop bound to use mat[i].length. Replace unsafe left-value check with explicit boundary checks and add prints for Right, Up and Down neighbors with appropriate index guards to avoid out-of-bounds access and to report adjacent values when the target element is found.

--- matrix_exercise/src/main/java/matrix/Main.java
+++ matrix_exercise/src/main/java/matrix/Main.java
@@ -9,7 +9,7 @@ public class Main {
         int M = sc.nextInt();
         int N = sc.nextInt();
 
-        int[][] mat = new int[M][N];
+        Integer[][] mat = new Integer[M][N];
 
         for (int i = 0; i < mat.length; i++) {
             for (int j = 0; j < mat[i].length; j++) {
@@ -18,12 +18,21 @@ public class Main {
         }
 
         int pos_matrix = sc.nextInt();
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat.length; j++) {
+        for (Integer i = 0; i < mat.length; i++) {
+            for (Integer j = 0; j < mat[i].length; j++) {
                 if (pos_matrix == mat[i][j]) {
                     System.out.println("Position: " + i  + "," + j);
-                    if (mat[i][j-1] != 0 ) {
+                    if (j > 0 ) {
                         System.out.println("Left: " + mat[i][j-1]);
+                    }
+                    if (j < mat.length) {
+                        System.out.println("Right: " + mat[i][j+1]);
+                    }
+                    if (i > 0){
+                        System.out.println("Up: " + mat[i-1][j]);
+                    }
+                    if (i < mat.length) {
+                        System.out.println("Down: " + mat[i+1][j]);
                     }
                 }
             }
