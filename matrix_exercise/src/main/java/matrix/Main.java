2680655034ff9804909fe08e02394e04b987559c
VitorVicenzo <vitorvicenzo70@gmail.com>
2026-04-01 18:03:58 -0300

    Test until Left position part.

--- matrix_exercise/src/main/java/matrix/Main.java
+++ matrix_exercise/src/main/java/matrix/Main.java
@@ -9,9 +9,27 @@ public class Main {
         int M = sc.nextInt();
         int N = sc.nextInt();
 
-        
+        int[][] mat = new int[M][N];
 
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
 
+        int pos_matrix = sc.nextInt();
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat.length; j++) {
+                if (pos_matrix == mat[i][j]) {
+                    System.out.println("Position: " + i  + "," + j);
+                    if (mat[i][j-1] != 0 ) {
+                        System.out.println("Left: " + mat[i][j-1]);
+                    }
+                }
+            }
+        }
         sc.close();
     }
+
+    
 }
